e3a7135c6021cb68f02fd96fb6fe550f1b40781b
Diyar Kudrat <dkudrat@gmail.com>
2020-08-26 16:43:16 -0700

    initial commit

---
+++ blockchain.py
@@ -0,0 +1,19 @@
+
+class Blockchain(object):
+    def __init__(self):
+
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+
+    @property
+    def last_block(self):
+        pass
