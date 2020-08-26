39968847815e268c9c721c40609dbff39def7d7d
Diyar Kudrat <dkudrat@gmail.com>
2020-08-26 16:52:45 -0700

    set up what's contained in a new transaction and a new block

--- blockchain.py
+++ blockchain.py
@@ -4,12 +4,32 @@ class Blockchain(object):
 
         self.chain = []
         self.current_transactions = []
+        self.new_block(previous_hash=1,proof=100)
 
     def new_block(self):
-        pass
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'proof': proof,
+            previous_hash: previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+
+        return block
 
     def new_transaction(self):
-        pass
+        
+        self.current_transactions.append(
+            {
+                'sender': sender,
+                'receiver': receiver,
+                'amount': amount,
+            }
+        )
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
