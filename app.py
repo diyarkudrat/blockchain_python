e574484df1dd0296b1baf11fe437cb81b61d1919
Diyar Kudrat <dkudrat@gmail.com>
2020-08-28 17:26:18 -0700

    logic for new transportation path

--- app.py
+++ app.py
@@ -15,8 +15,16 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
+    values = request.get_json()
 
-    return "adding a new transaction"
+    required = ['sender','recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'missing values!!', 400
+
+    index = blockchain.new_block(values['sender'],values['recipient'],values['amount'])
+    response = {'message': "Transaction is scheduled to be added to Block No. {}".format(index)}
+
+    return jsonify(response), 201
 
 @app.route('/chain', methods=["GET"])
 def full_chain():
